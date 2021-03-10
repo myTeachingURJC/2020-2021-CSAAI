ac15517fbd9c7e4fff1da94a170e116d4eca1b7c
Obijuan <obijuan.cube@gmail.com>
2021-03-10 12:01:53 +0100

    S4: Templated added

---
+++ S04/Examples/plantilla.js
@@ -0,0 +1,5 @@
+//-- Punto de entrada: una vez cargada la página se 
+//-- ejecuta este código javascript
+
+console.log("Aquí comienza tu código JS...")
+console.log("¡Que comiencen los juegos del JS!")
