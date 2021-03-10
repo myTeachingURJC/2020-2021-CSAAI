90eb88f8c103519b740b08738990989259066747
Obijuan <obijuan.cube@gmail.com>
2021-03-10 12:58:37 +0100

    S4: Example 8 added

---
+++ S04/Examples/Ej-08.js
@@ -0,0 +1,11 @@
+console.log("Ejecutando js...")
+
+//-- Leer el párrafo identificado como test
+const test = document.getElementById('test')
+
+//-- Configurar el manejador para el evento de
+//-- pulsación de botón
+test.onclick = () => {
+  console.log("Click sobre el párrafo...")
+}
+
