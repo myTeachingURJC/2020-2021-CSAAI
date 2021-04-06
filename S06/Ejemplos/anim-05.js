6b6d516718774738816ad2b81ba94dbda5d79a56
Obijuan <obijuan.cube@gmail.com>
2021-04-06 06:07:54 +0200

    S6: Example added

---
+++ S06/Ejemplos/anim-05.js
@@ -0,0 +1,59 @@
+console.log("Ejecutando JS...");
+
+const canvas = document.getElementById("canvas");
+
+//-- Definir el tamaño del canvas
+canvas.width = 300;
+canvas.height = 100;
+
+//-- Obtener el contexto del canvas
+const ctx = canvas.getContext("2d");
+
+//-- Coordenadas del objeto
+let x = 0;
+let y = 10;
+
+//-- Velocidades del objeto
+let velx = 3;
+let vely = 0.2;
+
+//-- Funcion principal de animacion
+function update() 
+{
+  console.log("test");
+  //-- Algoritmo de animacion:
+  //-- 1) Actualizar posicion del  elemento
+  //-- (física del movimiento rectilineo uniforme)
+
+   //-- Condicion de rebote en extremos del canvas
+   if (x < 0 || x >= (canvas.width - 20) ) {
+    velx = -velx;
+  }
+
+  //-- Actualizar la posición
+  x = x + velx;
+  y = y + vely;
+
+  //-- 2) Borrar el canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  //-- 3) Dibujar los elementos visibles
+  ctx.beginPath();
+    ctx.rect(x, y, 20, 20);
+
+    //-- Dibujar
+    ctx.fillStyle = 'red';
+
+    //-- Rellenar
+    ctx.fill();
+
+    //-- Dibujar el trazo
+    ctx.stroke()
+  ctx.closePath();
+
+  //-- 4) Volver a ejecutar update cuando toque
+  requestAnimationFrame(update);
+}
+
+//-- ¡Que empiece la función!
+update();
